3aeea9cad48ccb4807048f71fb0c1250b2d20eaa
ibrahimnazzier <ibrahimnazzier@gmail.com>
2023-08-18 18:02:01 +0300

    BL_Print_Message Func for debugging the bootloader.

--- Bootloader/bootloader.c
+++ Bootloader/bootloader.c
@@ -33,8 +33,37 @@
 
 
 /******************** Software Interfaces Decleration Starts ****************************************/
+/**
+ \brief Print Message over UART
+ \details set send message through UART used for Debugging 
+ \param [in] format specifiy the behavior of the variadic function
+ \param [...] Variadic function.
+ **/
+void BL_Print_Message(char *format, ...);
 
-/******************** Software Interfaces Decleration Starts ****************************************/
+/******************** Software Interfaces Decleration Ends ****************************************/
+
+/******************** Software Interfaces Definition Starts ****************************************/
+
+void BL_Print_Message(char *format, ...){
+	char Message[100] = {0};
+	va_list arg;
+		/* Enable Access to variable Arguments */
+	va_start(arg, format);
+		/* Write formatted data from variable argument list to string */
+	vsprintf(Message, format,arg);
+#if (BL_DEBUG_METHOD == BL_ENABLE_UART_DEBUG_MESSAGE)
+		/* Transmit formatted data through UART*/
+	HAL_UART_Transmit(BL_DEBUG_UART,(uint8_t*)Message, sizeof(Message), HAL_MAX_DELAY); 
+#elif (BL_DEBUG_METHOD == BL_ENABLE_SPI_DEBUG_MESSAGE)
+			/* Transmit formatted data through SPI*/
+#elif (BL_DEBUG_METHOD == BL_ENABLE_CAN_DEBUG_MESSAGE)
+				/* Transmit formatted data through CAN*/
+#endif
+		/* Perform clean up for an ap object initialized by a call to va start*/
+	va_end(arg);
+}
+
 
 /******************* global variables declerations ***************************************************/
 
--- Bootloader/bootloader.h
+++ Bootloader/bootloader.h
@@ -20,6 +20,12 @@
 
 
 /******************** Macro Decleration Starts ******************************************************/
+#define BL_ENABLE_UART_DEBUG_MESSAGE		0x00
+#define BL_ENABLE_SPI_DEBUG_MESSAGE 		0x01
+#define BL_ENABLE_CAN_DEBUG_MESSAGE		0x02
+#define BL_DEBUG_METHOD (BL_ENABLE_UART_DEBUG_MESSAGE)
+
+#define BL_DEBUG_UART 				&huart3
 
 /******************** Macro Decleration Ends ********************************************************/
 
@@ -38,9 +44,19 @@
 
 
 /******************** Software Interfaces Decleration Starts ****************************************/
+/**
+ \brief Print Message over UART
+ \details set send message through UART used for Debugging 
+ \param [in] format specifiy the behavior of the variadic function
+ \param [...] Variadic function.
+ **/
+
+void BL_Print_Message(char *format, ...);
 
 
-/******************** Software Interfaces Decleration Starts ****************************************/
+/******************** Software Interfaces Decleration Ends ****************************************/
+
+
 
 #endif /* BOOTLOADER_H */
  
